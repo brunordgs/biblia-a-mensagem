be9c703d47eb89a42470b8e9edaf625868c78551
Kevin Kirsten Lucas <kevinkirstenlucas@gmail.com>
2024-11-22 19:57:14 -0300

    Refactor: Organize Google Analytics scripts into a reusable component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/footer";
+import { MicrosoftClarityScript } from "@/scripts/microsoft-clarity";
+import { GoogleAnalyticsScript } from "@/scripts/google-analytics";
 
 const inter = Inter({ subsets: ["latin"], display: "swap" });
 
@@ -24,34 +26,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
-      <Script
-        id="microsoft-clarity"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_MICROSOFT_CLARITY_PROJECT_ID}");`,
-        }}
-      />
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-          `,
-        }}
-      />
+      <MicrosoftClarityScript />
+      <GoogleAnalyticsScript />
       <body
         className={cn(
           inter.className,
